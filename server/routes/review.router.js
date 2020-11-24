5b8e9e2b96f78a79c3451ddbe68ceb27afc720bd
Alex Campbell <alexbe.campbell@gmail.com>
2020-11-23 23:29:03 -0600

    reviews POST route created, connected to server.js and tested in POSTMAN

--- server/routes/review.router.js
+++ server/routes/review.router.js
@@ -8,7 +8,7 @@ const {
 /**
  * GET route template
  */
-router.get('/', rejectUnauthenticated, (req, res) => {
+router.get('/', (req, res) => {
   // GET route code here
   pool
     .query(`SELECT * FROM "reviews" ORDER BY "id" ASC;`)
@@ -26,6 +26,24 @@ router.get('/', rejectUnauthenticated, (req, res) => {
  */
 router.post('/', (req, res) => {
   // POST route code here
+  const newReview = req.body;
+  const queryText = `INSERT INTO "reviews" ("masks", "tables", "party_size", "sanitizer_offered", "menu", "comments")
+    VALUES ($1, $2, $3, $4, $5, $6)`;
+  const queryValues = [
+    newReview.masks,
+    newReview.tables,
+    newReview.party_size,
+    newReview.sanitizer_offered,
+    newReview.menu,
+    newReview.comments,
+  ];
+  pool
+    .query(queryText, queryValues)
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log('ERROR POSTING PLANT: ', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
