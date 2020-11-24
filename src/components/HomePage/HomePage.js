e7889386c949aae370ae3708739e73edb5a89cbe
Alex Campbell <alexbe.campbell@gmail.com>
2020-11-24 17:33:47 -0600

    added restaurant cards to Home page, adjusting grid layout

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import MapBoxComponent from '../MapBoxComponent/MapBoxComponent';
 import { Grid } from '@material-ui/core';
+import RestaurantItem from '../RestaurantItem/RestaurantItem';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
@@ -11,10 +12,46 @@ import { Grid } from '@material-ui/core';
 class HomePage extends Component {
   state = {};
 
+  componentDidMount() {
+    console.log('HELLO');
+    this.props.dispatch({
+      type: 'GET_RESTAURANTS',
+      payload: this.props.store.restaurants,
+    });
+  }
+
   render() {
+    const restaurantArray = this.props.store.restaurantReducer.map(
+      (item, index) => {
+        return (
+          <Grid item xs={3} key={index}>
+            <RestaurantItem restaurants={item} index={index} {...this.props} />
+          </Grid>
+        );
+      }
+    );
+
     return (
-      <Grid container spacing={3}>
-        <MapBoxComponent />
+      <Grid
+        container
+        spacing={6}
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid>
+          <MapBoxComponent />
+        </Grid>
+        <Grid
+          container
+          spacing={4}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          {' '}
+          {restaurantArray}
+        </Grid>
       </Grid>
     );
   }
