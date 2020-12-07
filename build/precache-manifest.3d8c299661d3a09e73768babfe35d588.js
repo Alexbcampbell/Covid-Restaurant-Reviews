f7406c940fdbdce6107dc8d1bb955c8e7ee11840
Alex Campbell <alexbe.campbell@gmail.com>
2020-12-07 16:26:03 -0600

    production build for client

---
+++ build/precache-manifest.3d8c299661d3a09e73768babfe35d588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6c47b130d8abba9d6949a4163334b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b86907999d5cf38a45c",
+    "url": "/static/css/main.1c437423.chunk.css"
+  },
+  {
+    "revision": "a8341b5175800670c1f6",
+    "url": "/static/js/2.fe960ae5.chunk.js"
+  },
+  {
+    "revision": "1e7ec43b130e7e6929d7fb8e1b208114",
+    "url": "/static/js/2.fe960ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b86907999d5cf38a45c",
+    "url": "/static/js/main.34c92e17.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "0175f03c9bf1d4e410f8b083280526da",
+    "url": "/static/media/kenny-luo-7IiPopynx18-unsplash.0175f03c.jpg"
+  }
+]);
