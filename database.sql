1c70f4b83d436a321b20c29708e6cb78a1eac440
Alex Campbell <alexbe.campbell@gmail.com>
2020-12-09 16:19:09 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -3,11 +3,11 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
+--CREATE TABLE "user" (
+  --  "id" SERIAL PRIMARY KEY,
+-- "username" VARCHAR (80) UNIQUE NOT NULL,
+  ----  "password" VARCHAR (1000) NOT NULL
+--);
 
 -- "covid_reviews" DB
 
@@ -50,11 +50,22 @@ CREATE TABLE "restaurants" (
 	"name" VARCHAR(80),
 	"street" VARCHAR(80),
 	"city" VARCHAR(80),
-	"state" VARCHAR(40)
+	"state" VARCHAR(40),
+	"longitude" FLOAT ,
+	"latitude" FLOAT 
 );
 
-INSERT INTO "restaurants" ("name", "street", "city", "state")
-	VALUES('El Maguey', '7831 N Oak Trafficway', 'Kansas City', 'MO')
+INSERT INTO "restaurants" ("name", "street", "city", "state", "longitude", "latitude" )
+	VALUES--('El Maguey', '7831 N Oak Trafficway', 'Kansas City', 'MO', '-94.5752517', '39.2365134'),
+	('Corvino Supper Club', '1830 Walnut St', 'Kansas City', 'MO', '-94.5826133', '39.0912818'),
+	('Majestic', '931 Broadway Blvd', 'Kansas City', 'MO', '-94.5879988', '39.1026518'),
+	('Joe''s Kansas City BBQ', '3002 W 47th Ave', 'Kansas City', 'KS', '-94.6207599', '39.0444538'),
+	('Slap''s BBQ', '553 Central Ave', 'Kansas City', 'KS', '-94.6244509', '39.1024739'),
+	('Arthur Bryant''s Barbeque', '1727 Brooklyn Ave', 'Kansas City', 'MO', '-94.5560646', '39.091478'),
+	('LC''s Bar-B-Q', '5800 Blue Pkwy', 'Kansas City', 'MO', '-94.517065', '39.034619'),
+	('Green Room Burgers & Beer', '4010 Pennsylvania Ave', 'Kansas City', 'MO', '-94.5932559', '39.0544808'),
+	('The Peanut', '418 West 9th Street', 'Kansas City', 'MO', '-94.5890556', '39.1038536'),
+	('Brown & Loe', '429 Walnut Street', 'Kansas City', 'MO', '-94.5815061', '39.1087135')
 
 ;
 
@@ -71,7 +82,7 @@ CREATE TABLE "restaurants_reviews" (
 );
 
 INSERT INTO "restaurants_reviews" ("restaurants_id", "reviews_id")
-	VALUES(1,1);
+	VALUES (4,15);--,(3,7),(4,8),(5,9),(6,11),(7,12),(8,13),(9,14),(10,15);
 
 
 
@@ -85,3 +96,4 @@ SELECT * FROM "restaurants"
     JOIN "reviews" ON "restaurants_reviews".reviews_id = "reviews".id
     WHERE "restaurants".id = 1;
   
+ UPDATE "reviews" SET "rating"=5 WHERE id=1; 
